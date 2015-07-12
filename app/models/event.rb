0ec15751497ee87224f95c876411cea8677470f0
Michelle Golle <michelleg54@gmail.com>
2015-07-12 16:45:31 -0600

    remove image validation from event temporarily

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ActiveRecord::Base
   validates :approved, inclusion: [true, false]
   validates :date, presence: true, allow_blank: false
   validates :start_time, presence: true, allow_blank: false
-  validates :image_id, presence: true
+  # validates :image_id, presence: true
   validates :venue_id, presence: true
 
   belongs_to :image
